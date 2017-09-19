ac8356bffb06e1a9ed4a5aa194d66f320ca6374b
AJ Ansel <Andrew.ansel6@gmail.com>
2017-09-19 13:58:12 -0700

    Completed session_api_util

---
+++ vibes/frontend/util/session_api_util.js
@@ -0,0 +1,29 @@
+export const getUsers = () => {
+  return $.ajax({
+    method: 'GET',
+    url: 'api/users'
+  });
+};
+
+export const postUser = (user) => {
+  return $.ajax({
+    method: 'POST',
+    url: 'api/users',
+    data: { user }
+  });
+};
+
+export const postSession = (user) => {
+  return $.ajax({
+    method: 'POST',
+    url: 'api/sessions',
+    data: { user }
+  });
+};
+
+export const deleteSession = () => {
+  return $.ajax({
+    method: 'POST',
+    url: 'api/sessions'
+  });
+};
