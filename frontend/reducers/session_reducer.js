575ecc2b67647a158422dfe443c4432425b63222
AJ Ansel <Andrew.ansel6@gmail.com>
2017-09-19 16:11:32 -0700

    Exported SessionReducer and froze state

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -3,6 +3,7 @@ import {RECEIVE_CURRENT_USER} from '../actions/session_actions';
 const _nullSession = {};
 
 const SessionReducer = (state = _nullSession, action) => {
+  Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
       const currentUser = action.currentUser;
@@ -11,3 +12,5 @@ const SessionReducer = (state = _nullSession, action) => {
       return state;
   }
 };
+
+export default SessionReducer;
