78f12080b845b3a0e012f8dd6f55466291d70eb8
AJ Ansel <Andrew.ansel6@gmail.com>
2017-09-19 16:07:26 -0700

    Created session reducer

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,1 +1,13 @@
 import {RECEIVE_CURRENT_USER} from '../actions/session_actions';
+
+const _nullSession = {};
+
+const SessionReducer = (state = _nullSession, action) => {
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      const currentUser = action.currentUser;
+      return Object.assign({}, { currentUser });
+    default:
+      return state;
+  }
+};
