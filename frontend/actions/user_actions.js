912f164bf3866b10459b17cc03e2100b4a777910
AJ Ansel <Andrew.ansel6@gmail.com>
2017-09-22 21:34:11 -0700

    Created user actions and user api util ajax request

---
+++ frontend/actions/user_actions.js
@@ -0,0 +1,16 @@
+import {searchUserDatabase} from '../util/users_api_util';
+
+export const RECEIVE_USER_SEARCH_RESULTS = "RECEIVE_USER_SEARCH_RESULTS";
+// export const RECEIVE_USER_SEARCH_RESULTS_ERRORS =
+//               "RECEIVE_USER_SEARCH_RESULTS_ERRORS";
+
+const receiveUserSearchResults = (users) => ({
+  type: RECEIVE_USER_SEARCH_RESULTS,
+  users
+});
+
+export const searchDatabase = (query) => (dispatch) => (
+  searchUserDatabase(query).then(
+    (users) => dispatch(receiveUserSearchResults(users))
+  )
+);
