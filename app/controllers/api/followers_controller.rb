933ae4b7984c03e9d83936bb3e4e5c9e10198515
AJ Ansel <Andrew.ansel6@gmail.com>
2017-09-23 11:56:29 -0700

    Updated my likes controller and made it render the post show. Thus added like info to the post show

--- app/controllers/api/followers_controller.rb
+++ app/controllers/api/followers_controller.rb
@@ -1,28 +1,27 @@
 class Api::FollowersController < ApplicationController
   def create
-    @follow = Follower.new(follow_params)
+    @follow = Follower.new
+    @follow.followee_id = params[:id]
+    @follow.follower_id = current_user.id
 
     if @follow.save
-      render :show
+      @user = @follow.followee
+      render 'api/users/show'
     else
-      render json: @follow.errors.full_messages, status: 422
+      render json: @follow.errors.full_messages, status: 401
     end
   end
 
   def destroy
-    @follow = Follower.find(params[:id])
+    @follow = Follower.find_by(follower_id: current_user.id, followee_id: params[:id])
 
-    if @follow && @follow.follower_id == current_user.id
-      @follow.destroy!
-      render :show
+    if @follow
+      @follow.destroy
+      @user = @follow.followee
+      render 'api/users/show'
     else
+      # This status is followly not correct
       render json: ["You can only destroy your follows"], status: 404
     end
-  end
-
-  private
-
-  def follow_params
-    params.require(:follow).permit(:follow_id, :followee_id)
   end
 end
--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,5 +1,9 @@
 if user
   json.extract! user, :id, :name, :username, :img_url
+  json.posts user.posts.count
+  json.followers user.followers.count
+  json.followees user.followees.count
+  json.followed_by_current_user !!user.followers.find_by(follower_id: current_user.id)
 else
   {}
 end
