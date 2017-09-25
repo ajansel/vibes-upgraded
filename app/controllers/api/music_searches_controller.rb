fc46a385bbd159d5f307c151f7e946106bf07b13
AJ Ansel <Andrew.ansel6@gmail.com>
2017-09-25 11:21:35 -0700

    Created new controller action to search for songs by album and changed the name of my view to render songs based on album or artist

--- app/controllers/api/music_searches_controller.rb
+++ app/controllers/api/music_searches_controller.rb
@@ -8,7 +8,12 @@ class Api::MusicSearchesController < ApplicationController
 
   def songs_by_artist
     @songs = Song.where(artist_id: search_params[:artist_id])
-    render :songs_by_artist
+    render :songs_by_artist_or_album
+  end
+
+  def songs_by_album
+    @songs = Song.where(album_id: search_params[:album_id])
+    render :songs_by_artist_or_album
   end
 
   private
