8fcc32e3f1635a94d881c0494e0c8168d76930ed
AJ Ansel <andrew.ansel6@gmail.com>
2018-05-13 16:30:57 -0700

    Setup ActionCable config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,9 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
+  config.web_socket_server_url = "wss://vibes-upgraded.herokuapp.com/cable" 
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://vibes-upgraded.herokuapp.com', 'https://vibes-upgraded.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
